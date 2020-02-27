9b7bece27f0651687200314f5cc81ee886f009cd
Felix <51464337+ashotofmilk@users.noreply.github.com>
2020-02-27 03:58:58 -0500

    update

--- Labs/Lab_04/HashTableLin.java
+++ Labs/Lab_04/HashTableLin.java
@@ -12,7 +12,9 @@ public class HashTableLin {
     public HashTableLin (int maxNum, double load){
         this.maxLoad = load;
         double minSize = maxNum/load;
-        
+        for (int i = 0; i < size; i++){
+            table[i] = -1; // fills table with dummy values
+        }
         
 
 
@@ -25,7 +27,11 @@ public class HashTableLin {
         if (!this.isIn(n)){
             nKeys++;
             int index = n%size;
+            while (table[index] != -1){ // linear probing
+                index = (index+1)%size;
+            }
             table[index] = n;
+
         }
         
 
@@ -35,19 +41,32 @@ public class HashTableLin {
     }
 
     private void rehash(){
-        int newSize = 2*size;
+        int newSize = 2*this.size;
         
 
         int[] rehashed = new int[newSize];
+        for (int i = 0; i < newSize; i++){
+            
+        }
     }
 
     public boolean isIn(int n){
-        if 
-        return true;
+        int index = n%size; // set starting index
+        for (int i = index; i < size; i++){
+            if (n == this.table[index]){
+                return true;
+            }
+        }
+        return false; 
     }
 
     public void printKeys(){
-        for 
+        for (int i = 0; i < size; i++){
+            if (table[i] != -1){
+                System.out.print(table[i]+" ");
+            } 
+            System.out.println("\n");
+        }
     }
 
     public void printKeysAndIndexes(){
@@ -63,9 +82,26 @@ public class HashTableLin {
     }
 
     public int nextPrime(double n){
-        while (true){
+        int prime = (int)n;
+        boolean isPrime = false;
 
+        if (prime == 1){
+            return 2;
         }
+        
+        while (true){
+            for (int i = prime; i>2; i-- ){
+                if (prime%i == 0){
+                    isPrime = false;
+                    break;
+                }
+                isPrime = true;
+            }
+
+            prime++;
+        }
+            
+        
     }
 
 
--- Labs/Lab_04/TestHashTable.java
+++ Labs/Lab_04/TestHashTable.java
@@ -1,9 +1,12 @@
 package Labs.Lab_04;
-
 /**
  * TestHashTable
  */
 public class TestHashTable {
 
+    public static void main(String[] args) {
+        HashTableLin hsl = new HashTableLin(99, 12.5);
+        System.out.println(hsl.nextPrime(7));
+    }
     
 }
