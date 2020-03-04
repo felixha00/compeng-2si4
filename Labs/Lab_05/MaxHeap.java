5d3118555dfde412a6891b32acfa79bdf4148826
Felix <51464337+ashotofmilk@users.noreply.github.com>
2020-03-04 00:06:31 -0500

    update lab 5

--- Labs/Lab_05/MaxHeap.java
+++ Labs/Lab_05/MaxHeap.java
@@ -9,14 +9,48 @@ public class MaxHeap {
     int items[]; 
 
     public MaxHeap (int size){
-
+        this.arrSize = size; 
+        this.heapSize = 0; 
+        
+        items = new int[this. + 1]; 
+        items[0] = Integer.MAX_VALUE; 
     }
 
     public MaxHeap(Integer[] someArray){
 
     }
 
-    
+    public void insert(int n){
+
+    }
+
+    private int deleteMax(){
+        return 0;
+    }
+
+    public String toString(){
+        return "";
+    }
+
+    public static void heapsort(Integer[] arrayToSort){
+        
+    }
+
+    public int getHeapSize(){
+        return heapSize;
+    }
+
+    public int getArrSize(){
+        return arrSize;
+    }
+
+
+
+
+
+
+
+
 
     
 }
---
+++ Labs/Lab_05/TestMaxHeap.java
@@ -0,0 +1,12 @@
+package Labs.Lab_05;
+
+/**
+ * TestMaxHeap
+ */
+public class TestMaxHeap {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
