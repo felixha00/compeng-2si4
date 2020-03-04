3ef2e6545fef6faafe21332e5a3a3afc367d2bf6
Felix <51464337+ashotofmilk@users.noreply.github.com>
2020-03-03 23:00:22 -0500

    started lab 5

---
+++ Labs/Lab_05/MaxHeap.java
@@ -0,0 +1,22 @@
+package Labs.Lab_05;
+
+/**
+ * MaxHeap
+ */
+public class MaxHeap {
+    int heapSize;
+    int arrSize; 
+    int items[]; 
+
+    public MaxHeap (int size){
+
+    }
+
+    public MaxHeap(Integer[] someArray){
+
+    }
+
+    
+
+    
+}
